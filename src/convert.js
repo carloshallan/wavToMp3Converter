8ccb7dfc2bca65637d5aa90a7338969e77f448cd
C.Hallan <38888731+carloshallan@users.noreply.github.com>
2023-04-30 21:55:40 -0300

    split audio by silence

--- src/convert.js
+++ src/convert.js
@@ -2,6 +2,60 @@ const fs = require('fs')
 const ffmpeg = require('fluent-ffmpeg')
 const {isFile, isWavFile, inputExtensionName, outputExtensionName} = require('./utils')
 
+async function splitAudioOnSilence(inputFile, outputDir) {
+    const silenceThreshold = -50 // Adjust this value according to your needs (in dB)
+    const silenceDuration = 1 // Duration of silence to be considered as a separator (in seconds)
+
+    return new Promise((resolve, reject) => {
+        let silenceStartTimes = []
+
+        ffmpeg(inputFile)
+            .outputOptions('-f', 'null', '-')
+            .audioFilters(`silencedetect=noise=${silenceThreshold}dB:d=${silenceDuration}`)
+            .on('stderr', (stderrLine) => {
+                const silenceMatch = stderrLine.match(/silence_start: (\d+(\.\d+)?)/)
+                if (silenceMatch) {
+                    silenceStartTimes.push(parseFloat(silenceMatch[1]))
+                }
+            })
+            .on('error', (err) => {
+                reject(err)
+            })
+            .on('end', async () => {
+                let prevSilenceEnd = 0
+                const promises = []
+
+                for (let i = 0; i < silenceStartTimes.length; i++) {
+                    const start = prevSilenceEnd
+                    const end = silenceStartTimes[i]
+                    const duration = end - start
+
+                    const outputFilename = `${outputDir}/split_${i}.mp3`
+                    const promise = new Promise((resolve, reject) => {
+                        ffmpeg(inputFile)
+                            .setStartTime(start)
+                            .setDuration(duration)
+                            .output(outputFilename)
+                            .on('error', (err) => {
+                                reject(err)
+                            })
+                            .on('end', () => {
+                                resolve(outputFilename)
+                            })
+                            .run()
+                    })
+
+                    promises.push(promise)
+                    prevSilenceEnd = end + silenceDuration
+                }
+
+                const splitFiles = await Promise.all(promises)
+                resolve(splitFiles)
+            })
+            .run()
+    })
+}
+
 function convertWavToMp3(filename) {
     return new Promise((resolve, reject) => {
         if (!isWavFile(filename)) throw new Error('This file is not a wav file.')
@@ -9,10 +63,10 @@ function convertWavToMp3(filename) {
         const outputFile = filename.replace(inputExtensionName, outputExtensionName)
 
         ffmpeg({source: filename})
-            .on("error", (err) => {
+            .on('error', (err) => {
                 reject(err)
             })
-            .on("end", () => {
+            .on('end', () => {
                 resolve(outputFile)
             })
             .save(outputFile)
@@ -28,8 +82,7 @@ function convert(mainDir) {
             const filepath = `${mainDir}/${filename}`
             if (isFile(filepath)) {
                 await convertWavToMp3(filepath)
-            }
-            else {
+            } else {
                 convert(filepath)
             }
         })
@@ -39,5 +92,6 @@ function convert(mainDir) {
 
 module.exports = {
     convert,
-    convertWavToMp3
+    convertWavToMp3,
+    splitAudioOnSilence
 }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 const path = require('path')
-const {convertWavToMp3} = require('./convert')
+const {convertWavToMp3, splitAudioOnSilence} = require('./convert')
 const {isFile} = require('./utils')
 const argv = process.argv
 const directory_to_convert = argv[2]
@@ -15,20 +15,21 @@ function run(mainDir, newPrefix) {
             const filepath = `${mainDir}/${filename}`
 
             if (!filepath.includes('.DS_Store'))
-            if (isFile(filepath)) {
-                if (prefix) {
-                    let ext = path.extname(filepath)
+                if (isFile(filepath)) {
+                    if (prefix) {
+                        let ext = path.extname(filepath)
 
-                    if (!ext) ext = '.wav'
-                    const newFilepath = `${mainDir}/${newPrefix}-${index + 1}${ext}`
+                        if (!ext) ext = '.wav'
+                        const newFilepath = `${mainDir}/${newPrefix}-${index + 1}${ext}`
 
-                    await fs.promises.rename(filepath, newFilepath)
-                    await convertWavToMp3(newFilepath)
+                        await fs.promises.rename(filepath, newFilepath)
+                        await convertWavToMp3(newFilepath)
+                        await splitAudioOnSilence(newFilepath, path.dirname(newFilepath))
+                    }
+                    await convertWavToMp3(filepath)
+                } else {
+                    run(filepath, newPrefix)
                 }
-                await convertWavToMp3(filepath)
-            } else {
-                run(filepath, newPrefix)
-            }
         })
     })
 }
